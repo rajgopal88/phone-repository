eff6928822a0e9ff54bc9aa8301cd7491a50466a
Raj Gopal <raj.gopal980@gmail.com>
2020-06-13 17:46:43 +0530

    Added state in ‘AddSubscriber’ and ‘App’ components

--- src/App.js
+++ src/App.js
@@ -3,24 +3,15 @@ import Header from './Header.js';
 import './App.css';
 
 class App extends Component {
-
-  clickHandler(message) {
-    alert(message);
-  } 
-
+  constructor()
+  {
+    super();
+    this.state = 
+    {
+        subscriberListToShow: []
+    }
+  }
   render() {
-    let subscriber = [
-      {
-        "id": 1,
-        "name": "Sneha",
-        "phone": "123456"
-      },
-      {
-        "id": 2,
-        "name": "Raj",
-        "phone": "7891011"
-      }
-    ]
     return (
       <div>
         <Header heading="Phone Directory" />
@@ -32,12 +23,12 @@ class App extends Component {
             <span className="grid-item phone-heading">Phone</span>
           </div>
           {
-            subscriber.map(x => {
+            this.state.subscriberListToShow.map(x => {
               return <div key={x.id} className="grid-container">
             <span className="grid-item">{x.name}</span>
             <span className="grid-item">{x.phone}</span>
             <span className="grid-item">
-              <button className="custom-btn del-btn" onClick={this.clickHandler.bind(this, "Delete Handler Clicked")}>Delete</button>
+              <button className="custom-btn del-btn" >Delete</button>
             </span>
             </div>
             })
